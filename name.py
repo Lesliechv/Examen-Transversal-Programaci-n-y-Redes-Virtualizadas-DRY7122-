ba2420d8be006a56f2eaa9db8f955ed73a86ef8a
Lesliechv <le.chavez@duocuc.cl>
2024-07-03 20:01:41 +0000

    Archvios ET

---
+++ name.py
@@ -0,0 +1,3 @@
+int1 = input ("Ingrese nombre del primer integrante: ")
+int2 = input ("Ingrese nombre del segundo integrante: ")
+print("Hola somos " + int1 + " y " + int2 + " integrantes del ET de programacion.")
