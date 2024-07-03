0f920ca585931731cdb43f8dd0e545a1ece380d4
Lesliechv <le.chavez@duocuc.cl>
2024-07-03 20:06:09 +0000

    Archivos ET

--- vlan.py
+++
@@ -1,5 +0,0 @@
-vlan = int (input("Ingrese numero de vlan: "))
-if vlan >= 1 and vlan <= 1005:
-    print("Corresponde a vlan de rango normal.") 
-elif vlan >= 1006 and vlan <= 4094:
-    print("Corresponde a vlan de rango extendido.")
